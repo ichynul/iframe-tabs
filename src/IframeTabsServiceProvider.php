3072d4d8595d99f6c334f28cc821f4cac6e2f84f
ichynul <ichynul@outlook.com>
2019-06-27 10:32:07 +0800

    fix

--- src/IframeTabsServiceProvider.php
+++ src/IframeTabsServiceProvider.php
@@ -66,7 +66,8 @@ class IframeTabsServiceProvider extends ServiceProvider
         $c = request('c', '');
 
         if (!$this->app->runningInConsole()
-            && !$c || !preg_match('/.*?admin:minify.*?/i', $c)) { // if run admin:minify in `admin/helpers/terminal/artisan`
+            && (!$c || !preg_match('/.*?admin:minify.*?/i', $c)) // if run admin:minify in `admin/helpers/terminal/artisan`
+        ) {
 
             Admin::booted(function () use ($layer_path) {
 
